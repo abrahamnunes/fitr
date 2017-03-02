371435d3d9b138003fccc48d4ed863829963214b
abrahamnunes <abrahamjnunes@gmail.com>
2017-03-02 19:41:54 -0400

    Reinstated utilities file.

---
+++ fitr/fitrutils.py
@@ -0,0 +1,19 @@
+#===============================================================================
+#
+#   UTILITY FUNCTIONS
+#       Things that are used across fitr modules
+#
+#===============================================================================
+import numpy as np
+
+def softmax(x):
+    xmax = np.max(x)
+    return np.exp(x-xmax)/np.sum(np.exp(x-xmax))
+
+def logsumexp(x):
+    """
+    Numerically stable logsumexp.
+    """
+    xmax = np.max(x)
+    y = xmax + np.log(np.sum(np.exp(x-xmax)))
+    return y
