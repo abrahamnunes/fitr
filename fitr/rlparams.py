14edc79d17db94a5d168505b52834442fcdc7451
abrahamnunes <abrahamjnunes@gmail.com>
2017-03-02 19:42:58 -0400

    Moved parameter objects and added distributions to sample for each.

---
+++ fitr/rlparams.py
@@ -0,0 +1,49 @@
+#===============================================================================
+#
+#   PARAMETERS
+#       Objects denoting parameters for simple use
+#
+#===============================================================================
+import scipy.stats 
+
+class Param(object):
+    def __init__(self, name=None, rng=None):
+        self.name  = name
+        self.range = rng
+        self.dist  = None
+
+class LearningRate(Param):
+    """
+    A learning rate object.
+    """
+    def __init__(self, name='Learning Rate', rng='unit'):
+        self.name = name
+        self.rng  = rng
+        self.dist = scipy.stats.beta(1.1, 1.1)
+
+class RewardSensitivity(Param):
+    """
+    A reward sensitivity object.
+    """
+    def __init__(self, name='Reward Sensitivity', rng='unit'):
+        self.name = name
+        self.rng  = rng
+        self.dist = scipy.stats.beta(1.1, 1.1)
+
+class MBMF_Balance(Param):
+    """
+    An object representing the parameter that balances model-based and model-free control.
+    """
+    def __init__(self, name='Model-Based Control Weight', rng='unit'):
+        self.name = name
+        self.rng  = rng
+        self.dist = scipy.stats.beta(1.1, 1.1)
+
+class ChoiceRandomness(Param):
+    """
+    An choice randomness parameter object
+    """
+    def __init__(self, name='Choice Randomness', rng='pos'):
+        self.name = name
+        self.rng  = rng
+        self.dist = scipy.stats.gamma(5, 1)
