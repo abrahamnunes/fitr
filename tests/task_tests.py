9b3e6e14357035f2b1da8e751db4a870f66fcc2c
abrahamnunes <abrahamjnunes@gmail.com>
2017-04-08 19:17:20 -0300

    Fixed task import in unit test

--- tests/task_tests.py
+++ tests/task_tests.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import fitr
+from fitr import tasks
 import numpy as np
 import scipy
 
@@ -8,7 +9,7 @@ def test_bandit():
 	lr = fitr.rlparams.LearningRate()
 	cr = fitr.rlparams.ChoiceRandomness()
 	group = fitr.rlparams.generate_group(params=[lr, cr], nsubjects=10)
-	task = fitr.tasks.bandit()
+	task = tasks.bandit()
 	res = task.simulate(ntrials=10, nsubjects=10)
 
 	res.cumreward_param_plot()
@@ -28,7 +29,7 @@ def test_twostep():
 	cr = fitr.rlparams.ChoiceRandomness()
 	w = fitr.rlparams.MBMF_Balance()
 	group = fitr.rlparams.generate_group(params=[lr, cr, w], nsubjects=5)
-	task = fitr.tasks.twostep()
+	task = tasks.twostep()
 	res = task.simulate(ntrials=5, nsubjects=5)
 
 	assert(len(res.params) == 3)
@@ -43,3 +44,10 @@ def test_twostep():
 	assert(np.shape(res.data_mcmc['S2'])[1] == 5)
 	assert(np.shape(res.data_mcmc['R'])[0] == 5)
 	assert(np.shape(res.data_mcmc['R'])[1] == 5)
+
+def test_action():
+	assert(type(tasks.action(x=np.array([0.8, 0.2]))) == int)
+
+def test_reward():
+	paths = np.array([0.25, 0.25, 0.25])
+	assert(type(tasks.reward(a=1, paths=paths)) == int)
