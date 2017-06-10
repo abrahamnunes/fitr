Added tests for loglikelihood functions in twostep module, and fixed plotting test errors.

fitr/fitrdata.py
@@ -107,7 +107,19 @@ class SyntheticData(object):
 
 
 
-    def cumreward_param_plot(self, alpha=0.9):
+    def cumreward_param_plot(self, alpha=0.9, show_figure=True, save_figure=False, filename='cumreward-param-plot-sim.pdf'):
+        """
+        Plots parameter values against cumulative reward
+
+        Parameters
+        ----------
+        show_figure : bool
+            Whether to show the figure
+        save_figure : bool
+            Whether to save the figure to disk
+        filename : str
+            The name of the file to which to save the figure
+        """
         if self.params is not None:
             nsubjects = np.shape(self.params)[0]
             creward = np.zeros(nsubjects)
@@ -122,13 +134,29 @@ class SyntheticData(object):
                 ax[i].set_ylabel('Total Reward')
 
             plt.suptitle('Cumulative Reward vs. Parameters')
-            plt.show()
+
+            if save_figure is True:
+                plt.savefig(filename, bbox_inches='tight')
+
+            if show_figure is True:
+                plt.show()
         else:
             print('ERROR: There are no parameters assigned')
             return
 
-    def plot_cumreward(self):
-        """ Plots cumulative reward over time for each subject"""
+    def plot_cumreward(self, show_figure=True, save_figure=False, filename='cumreward-plot-sim.pdf'):
+        """
+        Plots cumulative reward over time for each subject
+
+        Parameters
+        ----------
+        show_figure : bool
+            Whether to show the figure
+        save_figure : bool
+            Whether to save the figure to disk
+        filename : str
+            The name of the file to which to save the figure
+        """
         nsubjects = len(self.data)
         fig, ax = plt.subplots(1, 1)
         for i in range(nsubjects):
@@ -138,4 +166,9 @@ class SyntheticData(object):
         ax.set_title('Cumulative Reward by Subject\n')
         ax.set_xlabel('Trial')
         ax.set_ylabel('Reward')
-        plt.show()
+
+        if save_figure is True:
+            plt.savefig(filename, bbox_inches='tight')
+
+        if show_figure is True:
+            plt.show()

tests/task_tests.py
@@ -32,7 +32,11 @@ def test_twostep_lr_cr_mf():
 	ntrials = 10
 
 	res = twostep.lr_cr_mf().simulate(ntrials=ntrials, nsubjects=nsubjects)
-
+	LL = twostep.lr_cr_mf().loglikelihood(params=res.params[0],
+										  states=res.data[0]['S'],
+										  actions=res.data[0]['A'],
+										  rewards=res.data[0]['R'])
+	assert(type(LL) is np.float64)
 	assert(len(res.data) == nsubjects)
 	assert(res.data_mcmc['N'] == nsubjects)
 	assert(res.data_mcmc['T'] == ntrials)
@@ -46,7 +50,11 @@ def test_twostep_lr_cr_rs_mf():
 	ntrials = 10
 
 	res = twostep.lr_cr_rs_mf().simulate(ntrials=ntrials, nsubjects=nsubjects)
-
+	LL = twostep.lr_cr_rs_mf().loglikelihood(params=res.params[0],
+										  	 states=res.data[0]['S'],
+										  	 actions=res.data[0]['A'],
+										  	 rewards=res.data[0]['R'])
+	assert(type(LL) is np.float64)
 	assert(len(res.data) == nsubjects)
 	assert(res.data_mcmc['N'] == nsubjects)
 	assert(res.data_mcmc['T'] == ntrials)
@@ -60,7 +68,11 @@ def test_twostep_lr_cr_et_mf():
 	ntrials = 10
 
 	res = twostep.lr_cr_et_mf().simulate(ntrials=ntrials, nsubjects=nsubjects)
-
+	LL = twostep.lr_cr_et_mf().loglikelihood(params=res.params[0],
+											 states=res.data[0]['S'],
+											 actions=res.data[0]['A'],
+											 rewards=res.data[0]['R'])
+	assert(type(LL) is np.float64)
 	assert(len(res.data) == nsubjects)
 	assert(res.data_mcmc['N'] == nsubjects)
 	assert(res.data_mcmc['T'] == ntrials)
@@ -103,6 +115,11 @@ def test_twostep_lr_cr_rs_p_mf():
 
 	res = twostep.lr_cr_rs_p_mf().simulate(ntrials=ntrials, nsubjects=nsubjects)
 
+	LL = twostep.lr_cr_rs_p_mf().loglikelihood(params=res.params[0],
+										  	   states=res.data[0]['S'],
+										  	   actions=res.data[0]['A'],
+										  	   rewards=res.data[0]['R'])
+	assert(type(LL) is np.float64)
 	assert(len(res.data) == nsubjects)
 	assert(res.data_mcmc['N'] == nsubjects)
 	assert(res.data_mcmc['T'] == ntrials)