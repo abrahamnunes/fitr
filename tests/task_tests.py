2ab6b50e1b232a90e374932f699daa6bc62f8a74
abrahamnunes <abrahamjnunes@gmail.com>
2017-06-10 11:15:38 -0300

    Updated task tests for new rlparams and tasks.

--- tests/task_tests.py
+++ tests/task_tests.py
@@ -27,11 +27,95 @@ def test_bandit():
 	assert(np.shape(res.data_mcmc['A']) == (ntrials, nsubjects))
 	assert(np.shape(res.data_mcmc['R']) == (ntrials, nsubjects))
 
-def test_twostep_mf():
+def test_twostep_lr_cr_mf():
 	nsubjects = 5
 	ntrials = 10
 
 	res = twostep.lr_cr_mf().simulate(ntrials=ntrials, nsubjects=nsubjects)
+
+	assert(len(res.data) == nsubjects)
+	assert(res.data_mcmc['N'] == nsubjects)
+	assert(res.data_mcmc['T'] == ntrials)
+	assert(np.shape(res.data_mcmc['A1']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['A2']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['S2']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['R']) == (ntrials, nsubjects))
+
+def test_twostep_lr_cr_rs_mf():
+	nsubjects = 5
+	ntrials = 10
+
+	res = twostep.lr_cr_rs_mf().simulate(ntrials=ntrials, nsubjects=nsubjects)
+
+	assert(len(res.data) == nsubjects)
+	assert(res.data_mcmc['N'] == nsubjects)
+	assert(res.data_mcmc['T'] == ntrials)
+	assert(np.shape(res.data_mcmc['A1']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['A2']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['S2']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['R']) == (ntrials, nsubjects))
+
+def test_twostep_lr_cr_et_mf():
+	nsubjects = 5
+	ntrials = 10
+
+	res = twostep.lr_cr_et_mf().simulate(ntrials=ntrials, nsubjects=nsubjects)
+
+	assert(len(res.data) == nsubjects)
+	assert(res.data_mcmc['N'] == nsubjects)
+	assert(res.data_mcmc['T'] == ntrials)
+	assert(np.shape(res.data_mcmc['A1']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['A2']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['S2']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['R']) == (ntrials, nsubjects))
+
+def test_twostep_lr_cr_p_mf():
+	nsubjects = 5
+	ntrials = 10
+
+	res = twostep.lr_cr_p_mf().simulate(ntrials=ntrials, nsubjects=nsubjects)
+
+	assert(len(res.data) == nsubjects)
+	assert(res.data_mcmc['N'] == nsubjects)
+	assert(res.data_mcmc['T'] == ntrials)
+	assert(np.shape(res.data_mcmc['A1']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['A2']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['S2']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['R']) == (ntrials, nsubjects))
+
+def test_twostep_lr_cr_et_p_mf():
+	nsubjects = 5
+	ntrials = 10
+
+	res = twostep.lr_cr_et_p_mf().simulate(ntrials=ntrials, nsubjects=nsubjects)
+
+	assert(len(res.data) == nsubjects)
+	assert(res.data_mcmc['N'] == nsubjects)
+	assert(res.data_mcmc['T'] == ntrials)
+	assert(np.shape(res.data_mcmc['A1']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['A2']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['S2']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['R']) == (ntrials, nsubjects))
+
+def test_twostep_lr_cr_rs_p_mf():
+	nsubjects = 5
+	ntrials = 10
+
+	res = twostep.lr_cr_rs_p_mf().simulate(ntrials=ntrials, nsubjects=nsubjects)
+
+	assert(len(res.data) == nsubjects)
+	assert(res.data_mcmc['N'] == nsubjects)
+	assert(res.data_mcmc['T'] == ntrials)
+	assert(np.shape(res.data_mcmc['A1']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['A2']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['S2']) == (ntrials, nsubjects))
+	assert(np.shape(res.data_mcmc['R']) == (ntrials, nsubjects))
+
+def test_twostep_lr_cr_rs_et_p_mf():
+	nsubjects = 5
+	ntrials = 10
+
+	res = twostep.lr_cr_rs_et_p_mf().simulate(ntrials=ntrials, nsubjects=nsubjects)
 
 	assert(len(res.data) == nsubjects)
 	assert(res.data_mcmc['N'] == nsubjects)
