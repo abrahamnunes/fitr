a3c684b01ef97280e6da68741b137ad10bf070ae
abrahamnunes <abrahamjnunes@gmail.com>
2017-06-19 18:26:31 -0300

    Updated unit tests for new two-step task models

--- tests/task_tests.py
+++ tests/task_tests.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import fitr
-from fitr.models import twostep
+from fitr.models import twostep as ts
 from fitr.models import driftbandit as db
 import numpy as np
 import scipy
@@ -126,15 +126,15 @@ def test_twostep_lr_cr_mf():
 	for rpath_common in [False, True]:
 		preset_paths = np.random.uniform(0, 1, size=[ntrials, 4, nsubjects])
 		for preset in [preset_paths, None]:
-			res = twostep.lr_cr_mf().simulate(ntrials=ntrials,
-											  nsubjects=nsubjects,
-											  group_id='A',
-											  rpath_common=rpath_common,
-											  preset_rpaths=preset)
-			LL = twostep.lr_cr_mf().loglikelihood(params=res.params[0],
-												  states=res.data[0]['S'],
-												  actions=res.data[0]['A'],
-												  rewards=res.data[0]['R'])
+			res = ts.lr_cr_mf().simulate(ntrials=ntrials,
+										 nsubjects=nsubjects,
+										 group_id='A',
+										 rpath_common=rpath_common,
+									     preset_rpaths=preset)
+			LL = ts.lr_cr_mf().loglikelihood(params=res.params[0],
+											 states=res.data[0]['S'],
+											 actions=res.data[0]['A'],
+											 rewards=res.data[0]['R'])
 			assert(type(LL) is np.float64)
 			assert(len(res.data) == nsubjects)
 			assert(res.data_mcmc['N'] == nsubjects)
@@ -151,15 +151,15 @@ def test_twostep_lr_cr_rs_mf():
 	for rpath_common in [False, True]:
 		preset_paths = np.random.uniform(0, 1, size=[ntrials, 4, nsubjects])
 		for preset in [preset_paths, None]:
-			res = twostep.lr_cr_rs_mf().simulate(ntrials=ntrials,
-												 nsubjects=nsubjects,
-												 group_id='A',
-												 rpath_common=rpath_common,
-												 preset_rpaths=preset)
-			LL = twostep.lr_cr_rs_mf().loglikelihood(params=res.params[0],
-												  	 states=res.data[0]['S'],
-												  	 actions=res.data[0]['A'],
-												  	 rewards=res.data[0]['R'])
+			res = ts.lr_cr_rs_mf().simulate(ntrials=ntrials,
+											nsubjects=nsubjects,
+											group_id='A',
+											rpath_common=rpath_common,
+											preset_rpaths=preset)
+			LL = ts.lr_cr_rs_mf().loglikelihood(params=res.params[0],
+											  	states=res.data[0]['S'],
+											  	actions=res.data[0]['A'],
+											  	rewards=res.data[0]['R'])
 			assert(type(LL) is np.float64)
 			assert(len(res.data) == nsubjects)
 			assert(res.data_mcmc['N'] == nsubjects)
@@ -176,15 +176,15 @@ def test_twostep_lr_cr_et_mf():
 	for rpath_common in [False, True]:
 		preset_paths = np.random.uniform(0, 1, size=[ntrials, 4, nsubjects])
 		for preset in [preset_paths, None]:
-			res = twostep.lr_cr_et_mf().simulate(ntrials=ntrials,
-												 nsubjects=nsubjects,
-												 group_id='A',
-												 rpath_common=rpath_common,
-												 preset_rpaths=preset)
-			LL = twostep.lr_cr_et_mf().loglikelihood(params=res.params[0],
-													 states=res.data[0]['S'],
-													 actions=res.data[0]['A'],
-													 rewards=res.data[0]['R'])
+			res = ts.lr_cr_et_mf().simulate(ntrials=ntrials,
+											nsubjects=nsubjects,
+											group_id='A',
+											rpath_common=rpath_common,
+											preset_rpaths=preset)
+			LL = ts.lr_cr_et_mf().loglikelihood(params=res.params[0],
+												states=res.data[0]['S'],
+												actions=res.data[0]['A'],
+												rewards=res.data[0]['R'])
 			assert(type(LL) is np.float64)
 			assert(len(res.data) == nsubjects)
 			assert(res.data_mcmc['N'] == nsubjects)
@@ -201,15 +201,15 @@ def test_twostep_lr_cr_p_mf():
 	for rpath_common in [False, True]:
 		preset_paths = np.random.uniform(0, 1, size=[ntrials, 4, nsubjects])
 		for preset in [preset_paths, None]:
-			res = twostep.lr_cr_p_mf().simulate(ntrials=ntrials,
-												nsubjects=nsubjects,
-												group_id='A',
-												rpath_common=rpath_common,
-												preset_rpaths=preset)
-			LL = twostep.lr_cr_p_mf().loglikelihood(params=res.params[0],
-												  	states=res.data[0]['S'],
-												  	actions=res.data[0]['A'],
-												  	rewards=res.data[0]['R'])
+			res = ts.lr_cr_p_mf().simulate(ntrials=ntrials,
+										   nsubjects=nsubjects,
+										   group_id='A',
+										   rpath_common=rpath_common,
+										   preset_rpaths=preset)
+			LL = ts.lr_cr_p_mf().loglikelihood(params=res.params[0],
+											   states=res.data[0]['S'],
+											   actions=res.data[0]['A'],
+											   rewards=res.data[0]['R'])
 			assert(type(LL) is np.float64)
 			assert(len(res.data) == nsubjects)
 			assert(res.data_mcmc['N'] == nsubjects)
@@ -226,15 +226,15 @@ def test_twostep_lr_cr_et_p_mf():
 	for rpath_common in [False, True]:
 		preset_paths = np.random.uniform(0, 1, size=[ntrials, 4, nsubjects])
 		for preset in [preset_paths, None]:
-			res = twostep.lr_cr_et_p_mf().simulate(ntrials=ntrials,
-												   nsubjects=nsubjects,
-												   group_id='A',
-												   rpath_common=rpath_common,
-												   preset_rpaths=preset)
-			LL = twostep.lr_cr_et_p_mf().loglikelihood(params=res.params[0],
-												  	   states=res.data[0]['S'],
-												  	   actions=res.data[0]['A'],
-												  	   rewards=res.data[0]['R'])
+			res = ts.lr_cr_et_p_mf().simulate(ntrials=ntrials,
+											  nsubjects=nsubjects,
+											  group_id='A',
+											  rpath_common=rpath_common,
+											  preset_rpaths=preset)
+			LL = ts.lr_cr_et_p_mf().loglikelihood(params=res.params[0],
+											  	  states=res.data[0]['S'],
+											  	  actions=res.data[0]['A'],
+											  	  rewards=res.data[0]['R'])
 			assert(type(LL) is np.float64)
 			assert(len(res.data) == nsubjects)
 			assert(res.data_mcmc['N'] == nsubjects)
@@ -251,16 +251,16 @@ def test_twostep_lr_cr_rs_p_mf():
 	for rpath_common in [False, True]:
 		preset_paths = np.random.uniform(0, 1, size=[ntrials, 4, nsubjects])
 		for preset in [preset_paths, None]:
-			res = twostep.lr_cr_rs_p_mf().simulate(ntrials=ntrials,
-												   nsubjects=nsubjects,
-												   group_id='A',
-												   rpath_common=rpath_common,
-												   preset_rpaths=preset)
+			res = ts.lr_cr_rs_p_mf().simulate(ntrials=ntrials,
+											  nsubjects=nsubjects,
+										      group_id='A',
+											  rpath_common=rpath_common,
+											  preset_rpaths=preset)
 
-			LL = twostep.lr_cr_rs_p_mf().loglikelihood(params=res.params[0],
-												  	   states=res.data[0]['S'],
-												  	   actions=res.data[0]['A'],
-												  	   rewards=res.data[0]['R'])
+			LL = ts.lr_cr_rs_p_mf().loglikelihood(params=res.params[0],
+												  states=res.data[0]['S'],
+											  	  actions=res.data[0]['A'],
+											  	  rewards=res.data[0]['R'])
 			assert(type(LL) is np.float64)
 			assert(len(res.data) == nsubjects)
 			assert(res.data_mcmc['N'] == nsubjects)
@@ -277,14 +277,145 @@ def test_twostep_lr_cr_rs_et_p_mf():
 	for rpath_common in [False, True]:
 		preset_paths = np.random.uniform(0, 1, size=[ntrials, 4, nsubjects])
 		for preset in [preset_paths, None]:
-			res = twostep.lr_cr_rs_et_p_mf().simulate(ntrials=ntrials,
-													  nsubjects=nsubjects,
-													  group_id='A',
-													  rpath_common=rpath_common,preset_rpaths=preset)
-			LL = twostep.lr_cr_rs_et_p_mf().loglikelihood(params=res.params[0],
-												  	      states=res.data[0]['S'],
-												  	      actions=res.data[0]['A'],
-												  	      rewards=res.data[0]['R'])
+			res = ts.lr_cr_rs_et_p_mf().simulate(ntrials=ntrials,
+												 nsubjects=nsubjects,
+												 group_id='A',
+												 rpath_common=rpath_common,
+												 preset_rpaths=preset)
+			LL = ts.lr_cr_rs_et_p_mf().loglikelihood(params=res.params[0],
+												  	 states=res.data[0]['S'],
+												  	 actions=res.data[0]['A'],
+												  	 rewards=res.data[0]['R'])
+
+			assert(type(LL) is np.float64)
+			assert(len(res.data) == nsubjects)
+			assert(res.data_mcmc['N'] == nsubjects)
+			assert(res.data_mcmc['T'] == ntrials)
+			assert(np.shape(res.data_mcmc['A1']) == (ntrials, nsubjects))
+			assert(np.shape(res.data_mcmc['A2']) == (ntrials, nsubjects))
+			assert(np.shape(res.data_mcmc['S2']) == (ntrials, nsubjects))
+			assert(np.shape(res.data_mcmc['R']) == (ntrials, nsubjects))
+
+def test_twostep_lr_cr_w():
+	nsubjects = 5
+	ntrials = 10
+
+	for rpath_common in [False, True]:
+		preset_paths = np.random.uniform(0, 1, size=[ntrials, 4, nsubjects])
+		for preset in [preset_paths, None]:
+			res = ts.lr_cr_w().simulate(ntrials=ntrials,
+										nsubjects=nsubjects,
+										group_id='A',
+									    rpath_common=rpath_common,
+										preset_rpaths=preset)
+			LL = ts.lr_cr_w().loglikelihood(params=res.params[0],
+											states=res.data[0]['S'],
+											actions=res.data[0]['A'],
+											rewards=res.data[0]['R'])
+
+			assert(type(LL) is np.float64)
+			assert(len(res.data) == nsubjects)
+			assert(res.data_mcmc['N'] == nsubjects)
+			assert(res.data_mcmc['T'] == ntrials)
+			assert(np.shape(res.data_mcmc['A1']) == (ntrials, nsubjects))
+			assert(np.shape(res.data_mcmc['A2']) == (ntrials, nsubjects))
+			assert(np.shape(res.data_mcmc['S2']) == (ntrials, nsubjects))
+			assert(np.shape(res.data_mcmc['R']) == (ntrials, nsubjects))
+
+def test_twostep_lr_cr_et_w():
+	nsubjects = 5
+	ntrials = 10
+
+	for rpath_common in [False, True]:
+		preset_paths = np.random.uniform(0, 1, size=[ntrials, 4, nsubjects])
+		for preset in [preset_paths, None]:
+			res = ts.lr_cr_et_w().simulate(ntrials=ntrials,
+										   nsubjects=nsubjects,
+										   group_id='A',
+									       rpath_common=rpath_common,
+										   preset_rpaths=preset)
+			LL = ts.lr_cr_et_w().loglikelihood(params=res.params[0],
+											   states=res.data[0]['S'],
+											   actions=res.data[0]['A'],
+											   rewards=res.data[0]['R'])
+
+			assert(type(LL) is np.float64)
+			assert(len(res.data) == nsubjects)
+			assert(res.data_mcmc['N'] == nsubjects)
+			assert(res.data_mcmc['T'] == ntrials)
+			assert(np.shape(res.data_mcmc['A1']) == (ntrials, nsubjects))
+			assert(np.shape(res.data_mcmc['A2']) == (ntrials, nsubjects))
+			assert(np.shape(res.data_mcmc['S2']) == (ntrials, nsubjects))
+			assert(np.shape(res.data_mcmc['R']) == (ntrials, nsubjects))
+
+def test_twostep_lr_cr_p_w():
+	nsubjects = 5
+	ntrials = 10
+
+	for rpath_common in [False, True]:
+		preset_paths = np.random.uniform(0, 1, size=[ntrials, 4, nsubjects])
+		for preset in [preset_paths, None]:
+			res = ts.lr_cr_p_w().simulate(ntrials=ntrials,
+										  nsubjects=nsubjects,
+										  group_id='A',
+									      rpath_common=rpath_common,
+										  preset_rpaths=preset)
+			LL = ts.lr_cr_p_w().loglikelihood(params=res.params[0],
+											  states=res.data[0]['S'],
+											  actions=res.data[0]['A'],
+											  rewards=res.data[0]['R'])
+
+			assert(type(LL) is np.float64)
+			assert(len(res.data) == nsubjects)
+			assert(res.data_mcmc['N'] == nsubjects)
+			assert(res.data_mcmc['T'] == ntrials)
+			assert(np.shape(res.data_mcmc['A1']) == (ntrials, nsubjects))
+			assert(np.shape(res.data_mcmc['A2']) == (ntrials, nsubjects))
+			assert(np.shape(res.data_mcmc['S2']) == (ntrials, nsubjects))
+			assert(np.shape(res.data_mcmc['R']) == (ntrials, nsubjects))
+
+def test_twostep_lr_cr_et_p_w():
+	nsubjects = 5
+	ntrials = 10
+
+	for rpath_common in [False, True]:
+		preset_paths = np.random.uniform(0, 1, size=[ntrials, 4, nsubjects])
+		for preset in [preset_paths, None]:
+			res = ts.lr_cr_et_p_w().simulate(ntrials=ntrials,
+											 nsubjects=nsubjects,
+											 group_id='A',
+									         rpath_common=rpath_common,
+											 preset_rpaths=preset)
+			LL = ts.lr_cr_et_p_w().loglikelihood(params=res.params[0],
+												 states=res.data[0]['S'],
+												 actions=res.data[0]['A'],
+												 rewards=res.data[0]['R'])
+
+			assert(type(LL) is np.float64)
+			assert(len(res.data) == nsubjects)
+			assert(res.data_mcmc['N'] == nsubjects)
+			assert(res.data_mcmc['T'] == ntrials)
+			assert(np.shape(res.data_mcmc['A1']) == (ntrials, nsubjects))
+			assert(np.shape(res.data_mcmc['A2']) == (ntrials, nsubjects))
+			assert(np.shape(res.data_mcmc['S2']) == (ntrials, nsubjects))
+			assert(np.shape(res.data_mcmc['R']) == (ntrials, nsubjects))
+
+def test_twostep_lr_cr_rs_et_p_w():
+	nsubjects = 5
+	ntrials = 10
+
+	for rpath_common in [False, True]:
+		preset_paths = np.random.uniform(0, 1, size=[ntrials, 4, nsubjects])
+		for preset in [preset_paths, None]:
+			res = ts.lr_cr_rs_et_p_w().simulate(ntrials=ntrials,
+											    nsubjects=nsubjects,
+											    group_id='A',
+									    		rpath_common=rpath_common,
+											    preset_rpaths=preset)
+			LL = ts.lr_cr_rs_et_p_w().loglikelihood(params=res.params[0],
+												  	states=res.data[0]['S'],
+												  	actions=res.data[0]['A'],
+												  	rewards=res.data[0]['R'])
 
 			assert(type(LL) is np.float64)
 			assert(len(res.data) == nsubjects)
