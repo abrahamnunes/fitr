f3718f5f0c8e859bc7d63ac5b673b6e0fb6f7146
abrahamnunes <abrahamjnunes@gmail.com>
2017-08-15 13:05:05 -0300

    Fixed unit test error.

--- tests/ae_stats_tests.py
+++ tests/ae_stats_tests.py
@@ -6,13 +6,13 @@ from fitr.criticism.ae_stats import param_ttest
 
 def test_paramcorr():
 	n_params = 3
-	n_subjects = 3
+	n_subjects = 30
 
 	x = np.random.normal(0, 1, size=(n_subjects, n_params))
 	y = np.random.normal(0, 1, size=(n_subjects, n_params))
 
 	corrs = paramcorr(X=x, Y=y)
-	assert(np.shape(corrs) == (n_params, n_sujects))
+	assert(np.shape(corrs) == (n_params, n_subjects))
 
 def test_param_ttest():
 	n_params = 3
@@ -22,4 +22,4 @@ def test_param_ttest():
 	y = np.random.normal(0, 1, size=(n_subjects, n_params))
 
 	res = param_ttest(X=x, Y=y)
-	assert(np.shape(res) == (n_params, n_sujects))
+	assert(np.shape(res) == (n_params, n_subjects))
