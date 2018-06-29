0efc6dd4c1357b8fa66e4bb933ed0677cb106b65
Alexander Rudiuk <rudiuk@dal.ca>
2018-06-29 15:50:35 -0300

    Updated files for new pypi -updated setup to work with current pypi -update setup.cfg to only mention license -added codecov.yml to ignore setup files

--- setup.py
+++ setup.py
@@ -1,44 +1,35 @@
 from setuptools import setup, find_packages
-from codecs import open
-
-import os
 
 # Get path to directory this script is run from
-here = os.path.abspath(os.path.dirname(__file__))
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
 
 setup(name='fitr',
-      version='0.0.1',
+      version='0.0.2',
       author='Abraham Nunes',
       author_email='nunes@dal.ca',
-      description='Tools for Computational Psychiatry research.',
-      long_description=open('README.rst').read(),
-      url='https://github.com/ComputationalPsychiatry/fitr',
-      download_url='https://github.com/ARudiuk/ComputationalPsychiatry/tarball/0.0.1',
-      keywords=['reinforcement learning',
-                'computational psychiatry',
-                'python',
-                'model fitting'],
+      description='Tools for Computational Psychiatry Research',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/abrahamnunes/fitr',
+      keywords='reinforcement-learning computational-psychiatry model-fitting',
       classifiers=[
           # Maturity of project
+          #    2 - Pre-Alpha
           #    3 - Alpha
           #    4 - Beta
           #    5 - Production/Stable
           #    6 - Mature
           #    7 - Inactive
-          'Development Status :: 3 - Alpha',
+          'Development Status :: 2 - Pre-Alpha',
           'Intended Audience :: Science/Research',
           'Topic :: Scientific/Engineering',
           'Topic :: Scientific/Engineering :: Artificial Intelligence',
           'Topic :: Scientific/Engineering :: Medical Science Apps.',
-          'License :: OSI Approved :: MIT License',
+          'License::OSI Approved :: MIT License',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 2.7'
       ],
       license='MIT',
       packages=find_packages(exclude=['docs', 'examples', 'tests']),
-      package_data={'fitr.models': ['stancode/driftbandit/*.stan',
-                                    'stancode/twostep/*.stan']},
-      include_package_data=True
-      # zip_safe=False
       )
