99d1a8c80dfc393b579c11009e037a22555927e0
Abraham Nunes <abrahamjnunes@gmail.com>
2018-07-16 17:29:11 -0300

    Updated bms tests

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -1,26 +1,27 @@
+import pytest
 import numpy as np
 from fitr.inference import mlepar
-import pytest
+from fitr.inference import bms
 
 def test_bms_verbose():
     X = np.random.uniform(0, 200, size=(100, 10))
     pxp, xp, bor, q_m, alpha, f0, f1, niter = bms(X, verbose=True)
-    assert np.equal(pxp.sum(), 1)
-    assert np.equal(xp.sum(), 1)
+    assert np.equal(pxp.sum().round(5), 1)
+    assert np.equal(xp.sum().round(5), 1)
     assert (0 <= bor <= 1)
-    assert np.equal(q_m.sum(1), 1)
+    assert np.all(np.equal(q_m.sum(1).round(5), 1))
     assert np.all(np.greater(alpha, 0))
     assert np.greater_equal(f0, 0)
     assert np.greater_equal(f1, 0)
     assert np.greater(niter, 0)
 
-def test_bms_nptverbose():
+def test_bms_notverbose():
     X = np.random.uniform(0, 200, size=(100, 10))
     pxp, xp, bor, q_m, alpha, f0, f1, niter = bms(X, verbose=False)
-    assert np.equal(pxp.sum(), 1)
-    assert np.equal(xp.sum(), 1)
+    assert np.equal(pxp.sum().round(5), 1)
+    assert np.equal(xp.sum().round(5), 1)
     assert (0 <= bor <= 1)
-    assert np.equal(q_m.sum(1), 1)
+    assert np.all(np.equal(q_m.sum(1).round(5), 1))
     assert np.all(np.greater(alpha, 0))
     assert np.greater_equal(f0, 0)
     assert np.greater_equal(f1, 0)
